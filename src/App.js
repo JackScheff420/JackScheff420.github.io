6db87b9f7bbc56ddaf510ce9695a340fc23ab492
Maximilian Scheffler <Maximilian.Scheffler@alho.com>
2023-02-06 15:33:54 +0100

    Sidebar erstellt

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
+import Sidebar from './Sidebar';
 import './style/App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline bg-green-500">
-      Hello Max!
-    </h1>
-      
+      <div className='flex'>
+        <Sidebar></Sidebar>
+      </div>
     </div>
   );
 }
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,13 +1,53 @@
-import './style/sidebar.css'
 
 const Sidebar = props => {
 
     return (
-        <>
 
+        <nav class="fixed top-0 left-0 h-screen m-0 flex flex-col w-64 bg-gray-900 text-white shadow-lg">
+            <div class="flex-col flex-1 overflow-y-auto border-b border-white/20 text-center">
+                <a class="flex font-mono text-2xl bg-green-400" href="#">Maximilian Scheffler Website</a>
+            </div>
+            {/* Bottom Area */}
+            <div className="bottom-0">
+                <a class="flex py-3 px-3 items-center gap-3 rounded-md hover:bg-gray-500/10 transition-colors duration-200 text-white cursor-pointer text-sm">
+                    <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="h-4 w-4" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                        <polyline points="3 6 5 6 21 6"></polyline>
+                        <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+                        <line x1="10" y1="11" x2="10" y2="17"></line>
+                        <line x1="14" y1="11" x2="14" y2="17"></line>
+                    </svg>Instagram
+                </a>
+                <a class="flex py-3 px-3 items-center gap-3 rounded-md hover:bg-gray-500/10 transition-colors duration-200 text-white cursor-pointer text-sm">
+                    <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="h-4 w-4" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                        <polyline points="3 6 5 6 21 6"></polyline>
+                        <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+                        <line x1="10" y1="11" x2="10" y2="17"></line>
+                        <line x1="14" y1="11" x2="14" y2="17"></line>
+                    </svg>Linked-In
+                </a>
+                <a class="flex py-3 px-3 items-center gap-3 rounded-md hover:bg-gray-500/10 transition-colors duration-200 text-white cursor-pointer text-sm">
+                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 fill-current text-white">
+                        <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
+                    </svg>
+                    Github
+                </a>
+            </div>
+        </nav>
 
-        </>
     );
 }
 
 export default Sidebar;
+
+
+
+
+
+
+
+
+
+
+
+
+
